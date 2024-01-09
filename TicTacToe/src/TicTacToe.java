a156683699b993a2569497847466bcab6375b6d2
Anjani Sharma <137437629+anj1525@users.noreply.github.com>
2024-01-08 21:25:44 -0500

    Fixed Title
    Added a large title, centered it, started on the panel

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -6,7 +6,9 @@ public class TicTacToe {
  int boardwidth = 600;
  int boardheight = 650;
  
- JFrame board = new JFrame();
+ JFrame board = new JFrame("Tic Tac Toe");//names the window
+ JLabel tJLabel = new JLabel();
+ JPanel tPanel = new JPanel();
 
  TicTacToe(){
    //creating a board to build the game on top of 
@@ -16,5 +18,19 @@ public class TicTacToe {
     board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     board.setLayout(new BorderLayout());
 
+    tJLabel.setBackground(Color.darkGray); //set the background
+    tJLabel.setForeground(Color.white); //set font colour
+    tJLabel.setFont(new Font("Arial",Font.BOLD,50));
+    tJLabel.setHorizontalAlignment(JLabel.CENTER);
+    tJLabel.setText("Tic Tac Toe");
+    tJLabel.setOpaque(true);
+
+    tPanel.setLayout(new BorderLayout());
+    tPanel.add(tJLabel);
+    board.add(tPanel, BorderLayout.NORTH);
+
+    tPanel.setLayout(new GridLayout(3,3));
+    tPanel.setBackground(Color.darkGray);
+    board.add(tPanel);
  }
 }
